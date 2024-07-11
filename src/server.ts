1de3a137b825b44c12f7d0d3b85384d816ccd6a8
arthurwillame5 <arthurwillame2005@gmail.com>
2024-07-11 18:13:28 -0300

    feat: add zod to validation

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
 import fastify from 'fastify';
+import { CreateTrip } from './routes/create-trip';
+import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
 
 const app = fastify();
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(CreateTrip)
 
 app.listen({port: 3333}).then(() => {
     console.log("Server Running 🚀")
