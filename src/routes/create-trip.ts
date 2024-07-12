f3355789aaa0f2c1f3325584db89528329bdda15
arthurwillame5 <arthurwillame2005@gmail.com>
2024-07-12 16:15:54 -0300

    feat: add confirme trip route

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -1,10 +1,14 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import dayjs from "dayjs";
+import "dayjs/locale/pt-br"
 import z from "zod";
 import nodemailer from "nodemailer";
 import { prisma } from "../lib/prisma";
 import { getMailClient } from "../lib/mail";
+import dayjs from "dayjs";
+
+dayjs.locale("pt-br");
 
 export async function CreateTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -62,32 +66,34 @@ export async function CreateTrip(app: FastifyInstance) {
         },
       });
 
+      const formattedStartsAt = dayjs(starts_at).format("LL");
+      const formattedEndsAt = dayjs(ends_at).format("LL");
+    
+      const confirmationLink= `http://localhost:3333/trip/${trip.id}/confirm`;
+
       const mail = await getMailClient();
 
-    const message = await mail.sendMail({
-      from: {
-        name: "Equipe plann.er",
-        address: "equipe@planner.com",
-      },
-      to: {
-        name: owner_name,
-        address: owner_email,
-      },
-      subject: `confirme sua viagem para ${destination}`,
-      html: `
+      const message = await mail.sendMail({
+        from: {
+          name: "Equipe plann.er",
+          address: "equipe@planner.com",
+        },
+        to: {
+          name: owner_name,
+          address: owner_email,
+        },
+        subject: `confirme sua viagem para ${destination}`,
+        html: `
       <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6; ">
-    <p>
-      Você solicitou a criação de uma viagem para <strong>${destination}</strong> nas datas de <strong>${dayjs(starts_at).format('DD [de] MMMM [de] YYYY')}</strong> .</p>
+    <p>Você solicitou a criação de uma viagem para <strong>${destination}</strong> nas datas de <strong>${formattedStartsAt} a ${formattedEndsAt}</strong> .</p>
 <p></p>
-    <a href="#">
-      Para confirmar sua viagem, clique no link abaixo:
-    </a>
+    <a href="${confirmationLink}">Para confirmar sua viagem, clique no link abaixo:</a>
 <p></p>
     <p>Caso você não saiba do que se trata esse e-mail, apenas ignore.</p>
 </div>
       
       `.trim(),
-    });
+      });
 
       console.log(nodemailer.getTestMessageUrl(message));
 
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify';
 import { CreateTrip } from './routes/create-trip';
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
+import { ConfirmeTrip } from './routes/confirme-trips';
 
 const app = fastify();
 
@@ -8,6 +9,7 @@ app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
 app.register(CreateTrip)
+app.register(ConfirmeTrip)
 
 app.listen({port: 3333}).then(() => {
     console.log("Server Running 🚀")
