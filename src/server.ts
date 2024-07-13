06c35e70e6218f66bf45fc5bc74a28f2aff3ca71
arthurwillame5 <arthurwillame2005@gmail.com>
2024-07-13 17:10:47 -0300

    feat: Add route to create invite for a trip

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,10 @@ import { ConfirmParticipant } from './routes/confirm-participant';
 import { ConfirmTrip } from './routes/confirm-trips';
 import { CreateActivity } from './routes/create-activity';
 import { GetActivities } from './routes/get-activities';
-import { CreateLinks } from './routes/get-links';
+import { CreateLinks } from './routes/create-link';
+import { GetParticipants } from './routes/get-participants';
+import { createInvite } from './routes/create-invite';
+import { getLinks } from './routes/get-links';
 
 const app = fastify();
 
@@ -23,6 +26,9 @@ app.register(ConfirmParticipant)
 app.register(CreateActivity)
 app.register(GetActivities)
 app.register(CreateLinks)
+app.register(GetParticipants)
+app.register(createInvite)
+app.register(getLinks)
 
 app.listen({port: 3333}).then(() => {
     console.log("Server Running 🚀")
