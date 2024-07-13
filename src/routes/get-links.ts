9d1a78399d088e52e19743950aecb6ae3e36d1f8
arthurwillame5 <arthurwillame2005@gmail.com>
2024-07-13 17:11:04 -0300

    feat: Refactor getLinks route to use type imports and handle errors

--- src/routes/get-links.ts
+++ src/routes/get-links.ts
@@ -1,43 +1,34 @@
-import { FastifyInstance } from "fastify";
-import { ZodTypeProvider } from "fastify-type-provider-zod";
-import z from "zod";
-import { prisma } from "../lib/prisma";
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { prisma } from '../lib/prisma'
+import { ClientError } from '../errors/client-error'
 
-export async function CreateLinks(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().post(
-    "/trips/:tripId/links",
+export async function getLinks(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/trips/:tripId/links',
     {
       schema: {
         params: z.object({
           tripId: z.string().uuid(),
         }),
-        body: z.object({
-          title: z.string(),
-          url: z.string().url(),
-        }),
       },
     },
     async (request) => {
-      const { tripId } = request.params;
-      const { title, url } = request.body;
+      const { tripId } = request.params
 
       const trip = await prisma.trip.findUnique({
         where: { id: tripId },
-      });
+        include: { 
+          links: true,
+        },
+      })
 
       if (!trip) {
-        throw new Error("Trip not found");
+        throw new ClientError('Trip not found')
       }
 
-      const link = await prisma.link.create({
-        data: {
-          title,
-          url,
-          trip_id: tripId,
-        },
-      });
-
-      return { linkId: link.id };
-    }
-  );
+      return { links: trip.links }
+    },
+  )
 }
