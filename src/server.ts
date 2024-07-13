ad1c67c4f0da9f7d99722eaac8455d93a571ecfc
arthurwillame5 <arthurwillame2005@gmail.com>
2024-07-13 11:11:54 -0300

    feat: Refactor trip confirmation routes and email functionality

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import fastify from 'fastify';
 import { CreateTrip } from './routes/create-trip';
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
-import { ConfirmeTrip } from './routes/confirme-trips';
+import { ConfirmTrip, ConfirmTrip } from './routes/confirm-trips';
 import cors from "@fastify/cors"
 
 const app = fastify();
@@ -14,7 +14,7 @@ app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
 app.register(CreateTrip)
-app.register(ConfirmeTrip)
+app.register(ConfirmTrip)
 
 app.listen({port: 3333}).then(() => {
     console.log("Server Running 🚀")
