aa70799e82452a80c0389eda6323a2a6ba45f5b4
arthurwillame5 <arthurwillame2005@gmail.com>
2024-07-13 16:27:19 -0300

    feat: Refactor getActivities route to include activities by date

--- src/routes/get-activities.ts
+++ src/routes/get-activities.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
-import z from "zod";
+import z, { date } from "zod";
 import { prisma } from "../lib/prisma";
+import { dayjs } from "../lib/dayjs";
 
 export async function GetActivities(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().get(
@@ -17,15 +18,37 @@ export async function GetActivities(app: FastifyInstance) {
       const { tripId } = request.params;
 
       const trip = await prisma.trip.findUnique({
-        where: {id: tripId},
-        include: { activities: true }
-      })
+        where: { id: tripId },
+        include: {
+          activities: {
+            orderBy: { occurs_at: "asc" },
+          },
+        },
+      });
 
-      if(!trip) {
-        throw new Error("Trip not found")
+      if (!trip) {
+        throw new Error("Trip not found");
       }
 
-      return {activities: trip.activities};
+      const differenceInDaysBetweenTripStartAndEnd = dayjs(trip.ends_at).diff(
+        trip.starts_at,
+        "days"
+      );
+
+      const activities = Array.from({
+        length: differenceInDaysBetweenTripStartAndEnd + 1,
+      }).map((_, index) => {
+        const date = dayjs(trip.starts_at).add(index, "days");
+
+        return {
+          date: date.toDate(),
+          activities: trip.activities.filter((activity) => {
+            return dayjs(activity.occurs_at).isSame(date, "day");
+          }),
+        };
+      });
+
+      return { activities };
     }
   );
 }
